8da923365cccf05240bfc90a181cefaef437302f
serant <seran.thirugnanam@gmail.com>
2018-07-08 17:10:02 -0400

    Added preliminary logging and thread support for reading arduino messages

--- main.py
+++ main.py
@@ -2,38 +2,56 @@ from bluedot import BlueDot
 from signal import pause
 from serial.tools import list_ports
 import serial
+import logging
+import threading 
 
 # First find arduino (ACM0 type)
 devices = list(list_ports.comports())
 arduinoPort = ''
 for port in devices: 
+    print(port)
     if 'ACM0' in port[1]:
         arduinoPort = port[0]
-        
 
-print(arduinoPort)
+if not arduinoPort:
+    raise serial.SerialException('Cannot find port to connect to')
 
+logging.info('Found device at: ' + arduinoPort)
+ 
 ser = serial.Serial(arduinoPort, 9600)
-while True:
-    print ser.readline()
+
+# Beginng reading Daemon
+readThread = threading.Thread(target=readMessageLoop, args=(ser,))
 
 bd = BlueDot()
 
 def move(pos):
     if pos.top:
-        print('T,' + pos.distance)
+        sendCommand('T,' + pos.distance)
     elif pos.bottom:
-        print('B,' + pos.distance)
+        sendCommand('B,' + pos.distance)
     elif pos.left:
-        print('L,' + pos.distance)
+        sendCommand('L,' + pos.distance)
     elif pos.right:
-        print('R,' + pos.distance)
+        sendCommand('R,' + pos.distance)
 
 def stop():
-    print('S,')
+    sendCommand('S')
 
 bd.when_pressed = move
 bd.when_moved = move
 bd.when_released = stop
+
+def sendCommand(command): 
+    logging.info('Sent: ' + command)
+    return ser.write((command + '\r\n').encode())
+
+def readMessageLoop(ser):
+    while True:
+        message = ser.readline().decode()
+        handleMessage(message)
+
+def handleMessage(message):
+    logging.debug(message)
 
 pause()
